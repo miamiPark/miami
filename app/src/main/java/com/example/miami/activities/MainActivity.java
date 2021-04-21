41edf8c6e934e3131d67fb5b7532cd032a20a65c
andrey <andmavrin@yandex.ru>
2021-04-21 21:09:01 +0300

    layout name and bd fragments

--- app/src/main/java/com/example/miami/activities/MainActivity.java
+++ app/src/main/java/com/example/miami/activities/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.miami.R;
+import com.example.miami.fragments.Registration.NameFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.fragment_name);
+        setContentView(R.layout.fragment_date_birth);
     }
 }
