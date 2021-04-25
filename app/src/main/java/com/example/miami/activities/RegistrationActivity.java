20325d99812d5e4aed5ed884bcfad50e62627c18
Amaterasy17 <sheb70017@gmail.com>
2021-04-25 20:19:51 +0300

    have done reg_photo with preview

--- app/src/main/java/com/example/miami/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/miami/activities/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package com.example.miami.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -12,9 +14,12 @@ import com.example.miami.fragments.Registration.GenderPickerFragment;
 
 import com.example.miami.fragments.Registration.HeaderRegistrationFragment;
 import com.example.miami.fragments.Registration.IdentityFragment;
+import com.example.miami.fragments.Registration.PhotoFragment;
 
 
+import android.Manifest;
 import android.app.ActionBar;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import android.view.View;
@@ -23,6 +28,7 @@ import android.widget.Button;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -36,9 +42,18 @@ public class RegistrationActivity extends AppCompatActivity
         setContentView(R.layout.activity_registration);
 
         getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_registration, new PhotoFragment(), null)
                 .add(R.id.fragment_registration, new HeaderRegistrationFragment(), null)
-                .add(R.id.fragment_registration, new IdentityFragment(), null)
+//                .add(R.id.fragment_registration, new IdentityFragment(), null)
+
                 .commit();
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(RegistrationActivity.this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, 1);
+
+        }
     }
 
     @Override
@@ -54,4 +69,17 @@ public class RegistrationActivity extends AppCompatActivity
                 .replace(R.id.registration_view, new GenderPickerFragment(), null)
                 .commit();
     }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if(requestCode==1){
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
 }
